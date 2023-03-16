ebcbf3876b1ef28b334f87d0703a454cadfe70c7
cvetgeorgieva <125574029+cvetgeorgieva@users.noreply.github.com>
2023-03-16 14:09:07 +0200

    Add files via upload

---
+++ PS_42_Tsveti/PS_42_Tsveti/Program.cs
@@ -0,0 +1,26 @@
+﻿using PS_42_Tsveti.Model;
+using PS_42_Tsveti.View;
+using PS_42_Tsveti.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS_42_Tsveti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User();
+            user.Name = "Test"; 
+            user.Password = "test";
+            user.Role = Others.UserRolesEnum.STUDENT;
+            UserViewModel userViewModel = new UserViewModel(user);
+            UserView userView = new UserView(userViewModel);
+            userView.Display();
+
+        }
+    }
+}
---
+++ PS_42_Tsveti/WelcomeExtended/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WelcomeExtended
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Example 2
+                var user = new User
+                {
+                    Name = "John Smith",
+                    Password = "password123",
+                    Role = UserRolesEnum.Student
+                };
+                var viewModel = new UserViewModel(user);
+                var view = new UserView(viewModel);
+
+                view.Display();
+
+                //Throw error here
+                view.DisplayError();
+            }
+            catch (Exception e)
+            {
+                var log = new ActionOnError(log);
+                log(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executed in any case!");
+            }
+
+        }
+    }
+}
